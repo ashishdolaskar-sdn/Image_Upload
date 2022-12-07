596c7e057cf336c30374aa8a25f57564c81c277d
ashishdolaskar-sdn <ashishdolaskar121@gmail.com>
2022-12-07 10:35:37 +0530

    updated

--- src/controller/image.ts
+++ src/controller/image.ts
@@ -1,9 +1,12 @@
 import image from "../model/image";
 import { Response, Request } from "express";
+import mongoose from "mongoose";
 export const imageupload = async (req: Request, res: Response) => {
+  console.log(req.file?.filename,"ksjdfkdlj")
   const studentbody = new image({
     image: req.file?.filename,
   });
+
   const Data = await studentbody.save();
   try {
     res.status(200).json({
@@ -16,3 +19,31 @@ export const imageupload = async (req: Request, res: Response) => {
     });
   }
 };
+
+// export const getdatabyid = async (req: Request, res: Response) => {
+//   const { id } = req.params;
+//   console.log(id);
+//   const result = await image.findOne({
+//     _id: new mongoose.Types.ObjectId(id),
+//   });
+//   console.log(result);
+
+//   if (result) {
+//     res.send({
+//       status: true,
+//       message: "Data fetched",
+//       result: result,
+//     });
+//   }
+// };
+
+//  export const getdatabyid = async (req: Request, res: Response) => {
+//   var item = req.body;
+//   var params = { Bucket: req.params.todoprnlibucket, Key: 'keyname'}; // keyname can be a filename
+//   s3config.getObject(params, function (err: any, data: any) {
+//       if (err) {
+//           return res.send({ "error": err });
+//       }
+//       res.send({ data });
+//   });
+// }
--- src/route/s3bucket.ts
+++ src/route/s3bucket.ts
@@ -3,4 +3,5 @@ import {imageupload} from "../controller/image"
 import { upload } from "../middleware/s3bucket";
 const s3Router = express.Router();
 s3Router.post("/s3image", upload.single("image"), imageupload);
+// s3Router.get("/getimage",getdatabyid)
 export default s3Router;
